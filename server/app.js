a0c708b267c5c9b3d32bd142381a997290daaf15
lucasmmaidana <lucasmaidanasoy@gmail.com>
2021-02-12 20:16:12 -0300

    Server

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const { isValid } = require("./script")
+
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.post("/api", (req, res) => {
+  const string = req.body.string
+  const result = isValid(string)
+
+  console.log(result)
+  res.json({ isValid: result })
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
---
+++ server/script.js
@@ -0,0 +1,27 @@
+function isValid(string) {
+  const appearances = [...string].reduce((ac, current) => {
+    ac[current] = ac[current] ? ac[current] + 1 : 1
+    return ac
+  }, {})
+  const appearancesArray = Object.values(appearances)
+  const appearancesArraySorted = appearancesArray.sort((a, b) => a - b)
+
+  const lf = appearancesArraySorted[0] // Least frequent character
+
+  const a = appearancesArraySorted.filter((x) => x !== lf)
+
+  if (a.length > 1) {
+    // Need to remove more than 1 different characters
+    return false
+  } else {
+    if (a[0] - lf > 1) {
+      // // Need to remove more than 1 of the same character
+      return false
+    } else {
+      // I can remove 1 character
+      return true
+    }
+  }
+}
+
+module.exports = { isValid }
