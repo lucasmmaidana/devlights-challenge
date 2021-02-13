98fa141de2d03b98f2d4850520911995918cb939
lucasmmaidana <lucasmaidanasoy@gmail.com>
2021-02-13 14:05:28 -0300

    Add Form component

---
+++ client/devlights-app/src/components/Form.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Box, Flex, Input, Button } from '@chakra-ui/react';
+
+function Form({ onSubmitFunction }) {
+  return (
+    <form onSubmit={onSubmitFunction}>
+      <Flex align="center" my={4}>
+        <Box flex="1" p="2">
+          <Input required name="string" placeholder="Enter string" />
+        </Box>
+
+        <Box>
+          <Button type="submit" colorScheme="teal" mr="4">
+            Submit
+          </Button>
+        </Box>
+      </Flex>
+    </form>
+  );
+}
+
+export default Form;
