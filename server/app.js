0c4a35e96dbd1c2b33ea1d0456f050a9c3095cdb
lucasmmaidana <lucasmaidanasoy@gmail.com>
2021-02-13 11:43:46 -0300

    Update server port

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const { isValid } = require("./script")
 
 const express = require("express")
 const app = express()
-const port = 3000
+const port = 3001
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
