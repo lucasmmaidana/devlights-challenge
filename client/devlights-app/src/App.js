b3698a7c69da8bc76046091a4be08c1f6bdf79d6
lucasmmaidana <lucasmaidanasoy@gmail.com>
2021-02-13 12:39:46 -0300

    Add client app structure

--- client/devlights-app/src/App.js
+++ client/devlights-app/src/App.js
@@ -1,40 +1,104 @@
 import React from 'react';
+import { useState } from 'react';
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
+  Badge,
+  Center,
+  Heading,
+  Flex,
+  Input,
+  Button,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
   theme,
+  Divider,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
 function App() {
+  const [logsHistory, setLogsHistory] = useState([]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const string = event.target['string'].value;
+    console.log(event.target['string'].value);
+    fetch('/api', {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ string: string }),
+    })
+      .then(res => res.json())
+      .catch(error => console.error('Error:', error))
+      .then(result => {
+        let logDatetime = new Date();
+        logDatetime = logDatetime.toString().split(' ').splice(1, 4).join(' ');
+        setLogsHistory(prev => [
+          { string: string, isValid: result.isValid, datetime: logDatetime },
+          ...prev,
+        ]);
+        console.log(logsHistory);
+      });
+  }
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Center>
+        <Box width="100%" maxW="700px" minH="100vh" p={3}>
+          <Flex justify="flex-end" mb={10}>
+            <ColorModeSwitcher />
+          </Flex>
+          <form onSubmit={handleSubmit}>
+            <Flex align="center" my={4}>
+              <Box flex="1" p="2">
+                <Input required name="string" placeholder="Enter string" />
+              </Box>
+
+              <Box>
+                <Button type="submit" colorScheme="teal" mr="4">
+                  Submit
+                </Button>
+              </Box>
+            </Flex>
+          </form>
+          <Divider />
+          <Heading as="h4" size="lg" mt={4}>
+            History
+          </Heading>
+
+          <Table mt={5} variant="simple">
+            <Thead>
+              <Tr>
+                <Th>String</Th>
+                <Th>Valid</Th>
+                <Th>Datetime</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {logsHistory?.map(row => (
+                <Tr>
+                  <Td>{row.string}</Td>
+                  <Td>
+                    {row.isValid ? (
+                      <Badge colorScheme="green">Yes</Badge>
+                    ) : (
+                      <Badge colorScheme="red">No</Badge>
+                    )}
+                  </Td>
+                  <Td>{row.datetime}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </Box>
+      </Center>
     </ChakraProvider>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,16 +1,19 @@
 const { isValid } = require("./script")
 
 const express = require("express")
+const cors = require("cors")
 const app = express()
 const port = 3001
 
+app.use(cors())
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.post("/api", (req, res) => {
   const string = req.body.string
   const result = isValid(string)
-
+  res.setHeader("content-type", "application/json")
+  res.setHeader("Access-Control-Allow-Origin", "*")
   console.log(result)
   res.json({ isValid: result })
 })
