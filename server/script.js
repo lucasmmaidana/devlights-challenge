f771c3a96ea956fc27a57ce70b129ca1944653d3
lucasmmaidana <lucasmaidanasoy@gmail.com>
2021-02-16 16:21:10 -0300

    Add better constants names

--- server/script.js
+++ server/script.js
@@ -1,27 +1,28 @@
 function isValid(string) {
-  const appearances = [...string].reduce((ac, current) => {
+  const frequencies = [...string].reduce((ac, current) => {
     ac[current] = ac[current] ? ac[current] + 1 : 1
     return ac
   }, {})
-  const appearancesArray = Object.values(appearances)
-  const appearancesArraySorted = appearancesArray.sort((a, b) => a - b)
+  const frequenciesArray = Object.values(frequencies)
+  const frequenciesArraySorted = frequenciesArray.sort((a, b) => a - b)
 
-  const lf = appearancesArraySorted[0] // Least frequent character
+  const lowerFrequency = frequenciesArraySorted[0]
 
-  const a = appearancesArraySorted.filter((x) => x !== lf)
+  const moreFrequents = frequenciesArraySorted.filter(
+    (frequency) => frequency !== lowerFrequency
+  )
 
-  if (a.length > 1) {
-    // Need to remove more than 1 different characters
-    return false
+  if (moreFrequents.length === 0) {
+    // All characters appear the same number of times
+    return true
   } else {
-    if (a[0] - lf > 1) {
-      // Need to remove more than 1 of the same character
-      return false
-    } else {
-      // I can remove 1 character
+    if (moreFrequents.length === 1 && moreFrequents[0] - lowerFrequency === 1) {
+      // I can remove one of the same character
       return true
     }
   }
+
+  return false
 }
 
 module.exports = { isValid }
